4286f8932ddd5b4333f0cf8a240c7fbf7e187c74
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-09-14 21:29:36 -0400

    Amount field edgecases are set

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,13 +1,20 @@
 import Input from '../../UI/Input'
 import classes from './MealItemForm.module.css'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 const MealItemForm =(props)=>{
+  const [amountIsValid, setAmountIsValid] = useState(true)
   const amountInputRef = useRef()
 
   const submitHandler = event =>{
     event.preventDefault()
     const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
+      setAmountIsValid(false)
+      return;
+    }
   }
 
 
@@ -23,6 +30,7 @@ const MealItemForm =(props)=>{
       defaultValue: '1'
     }} />
     <button>+ Add</button>
+    {!amountIsValid && <p>Please enter a valid amount (1-5).</p> }
   </form>
 }
 
