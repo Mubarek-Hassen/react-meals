d641a5bbd94319004ff05417eb87e53044c6b1ec
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-09-15 16:00:36 -0400

    logic for cart item buttons added, needs fixing

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import classes from './Cart.module.css'
 import Modal from '../UI/Modal';
 import { useContext } from 'react';
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 const Cart = (props) =>{
   const cartCtx = useContext(CartContext)
@@ -10,9 +11,17 @@ const Cart = (props) =>{
 
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) =>{
+    cartCtx.removeItem(id)
+  }
+
+  const cartItemAddHandler = (item) =>{
+    cartCtx.addItem(item)
+  }
+
   const cartItems = <ul className={classes['cart-items']}>
     {cartCtx.items.map((item)=> (
-    <li>{item.name}</li>
+    <CartItem key= {item.id} name={item.name} amount= {item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)}  />
     ))}
     </ul>;
 
