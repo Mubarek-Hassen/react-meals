7b27504ab051313ea3d68df78328d752244d1bc2
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-11-17 20:54:36 -0500

    Loading state added

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -8,9 +8,11 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals =()=>{
 
   const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(()=>{
     const fetchMeals = async ()=>{ 
+      setIsLoading(true)
       const res = await fetch('https://react-http-62223-default-rtdb.firebaseio.com/meals.json');
       const data = await res.json()
       const loadedMeals = []
@@ -23,17 +25,24 @@ const AvailableMeals =()=>{
         })
       }
       setMeals(loadedMeals)
+      setIsLoading(false)
   }
   fetchMeals()
   },[])
+
+
+  if(isLoading){
+    return ( 
+    <section className={classes.MealsLoading}>
+      <h3>Loading...</h3>
+    </section>)
+  }
 
   const mealsList = meals.map(meal => <MealItem key={meal.id} name={meal.name} description = {meal.description} price = {meal.price} id={meal.id}/>)
 
   return <section className={classes.meals}>
     <Card>
-    <ul>
-      {mealsList}
-    </ul>
+    <ul>{mealsList}</ul>
     </Card>
   </section>
 };
