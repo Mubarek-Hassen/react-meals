6f0704cd814987c419e830cd7360b0ffdf815b66
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-11-17 20:37:16 -0500

    Fetch added

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,35 +1,29 @@
+import { useEffect } from 'react';
 import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-    {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-    },
-    {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-    },
-    {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-    },
-    {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-    },
-];
+
 
 const AvailableMeals =()=>{
+
+  useEffect(()=>{
+    const fetchMeals = async ()=>{ 
+      const res = await fetch('https://react-http-62223-default-rtdb.firebaseio.com/meals.json');
+      const data = await res.json()
+      const loadedMeals = []
+      for (const key in data){
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price
+        })
+      }
+
+  }
+  },[])
+
   const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} name={meal.name} description = {meal.description} price = {meal.price} id={meal.id}/>)
 
   return <section className={classes.meals}>
